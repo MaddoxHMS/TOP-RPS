b725df22f93ba2269b70f2502d91f4e6beb732e6
MaddoxHMS <adam.maddox@bisdmail.net>
2022-10-24 13:29:22 -0500

    Add score function to game and overall win condition
    retooled playRound to export object instead of string

--- index.js
+++ index.js
@@ -24,30 +24,74 @@ function playRound(playerChoice, ComputerChoice){
         paper: "Tie"
     };
 
+    let output = {};
+
     switch(playerChoice.toLowerCase()){
         case 'rock':
-            result = rock[ComputerChoice.toLowerCase()];
+            output.result = rock[ComputerChoice.toLowerCase()];
             break
         case 'scissors':
-            result = scissors[ComputerChoice.toLowerCase()];
+            output.result = scissors[ComputerChoice.toLowerCase()];
             break
         case 'paper':
-            result = paper[ComputerChoice.toLowerCase()];
+            output.result = paper[ComputerChoice.toLowerCase()];
             break
         default:
-            result ="Error in selection"
+            output.result = "Error in selection"
     };
 
+    if(output.result== "Win"){
+        output.phrase =`${playerChoice} beats ${ComputerChoice}!`;
+        output.playerScoreDelta = 1;
+        output.compScoreDelta = 0;
+    } else if(output.result == "Lose"){
+        output.phrase =`${ComputerChoice} beats ${playerChoice}!`;
+        output.playerScoreDelta = 0;
+        output.compScoreDelta = 1;
+    } else {
+        output.phrase = `Play again!`
+        output.playerScoreDelta = 0;
+        output.compScoreDelta = 0;
+    }
 
 
-
-    let phrase = result == "Win" ? `${playerChoice} beats ${ComputerChoice}!`
-                : result == "Lose" ? `${ComputerChoice} beats ${playerChoice}!`
+        output.phrase = output.result == "Win" ? `${playerChoice} beats ${ComputerChoice}!`
+                : output.result == "Lose" ? `${ComputerChoice} beats ${playerChoice}!`
                 : "Play Again!";
 
     console.log("Your Choice: ",playerChoice);
     console.log("Computer Choice: ",ComputerChoice);
-    return `You ${result}!!! ${phrase}`;
+    return output;
 }
 
-console.log(playRound(getComputerChoice(),getComputerChoice()));
+
+function game(){
+    console.log("Let's Play Rock-Paper-Scissors!!!")
+    playerScore = 0;
+    compScore = 0;
+    
+    for(let i = 1; i<6; i++) {
+        console.log(`****************`);
+        console.log(`Round ${i}`);
+        let playerChoice = prompt("Select Rock, Paper, or Scissors");
+        let currentRound = playRound(playerChoice,getComputerChoice());
+        playerScore += currentRound.playerScoreDelta;
+        compScore += currentRound.compScoreDelta;
+
+        console.log(`You ${currentRound.result}!!! ${currentRound.phrase}`);
+        console.log(`Current Score:
+        You: ${playerScore}  Bot: ${compScore}`);
+        
+
+    }
+    console.log(`****************`);console.log(`****************`);console.log(`****************`);
+    let overallResult = playerScore > compScore ? "Win"
+        : compScore > playerScore ? "Lose"
+        :"Tie";
+    console.log(`FINAL Score:
+        You: ${playerScore}  Bot: ${compScore}`);   
+    console.log(`You ${overallResult}!!!!!!!!!!`);
+    console.log("Thanks for playing!");
+}
+
+game();
